3c94f394ce66429b742235e6d2a4129d46fac52e
Daniel Torres <youkiddingd@gmail.com>
2019-10-11 16:09:42 -0500

    Creacion de los proyectos en el entorno virtual

--- CuentasxCobrar/CuentasxCobrar/wsgi.py
+++ CuentasxCobrar/CuentasxCobrar/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Contabilidad project.
+WSGI config for CuentasxCobrar project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Contabilidad.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CuentasxCobrar.settings')
 
 application = get_wsgi_application()
