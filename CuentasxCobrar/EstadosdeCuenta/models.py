e1220290ff0baaec564c4f7f6740d1ca600e9261
Daniel Torres <youkiddingd@gmail.com>
2019-11-23 12:08:45 -0600

    Tablas de cobros

--- CuentasxCobrar/EstadosdeCuenta/models.py
+++ CuentasxCobrar/EstadosdeCuenta/models.py
@@ -122,3 +122,40 @@ class RelacionConceptoxProyecto(models.Model):
     class Meta:
         db_table="RelacionConceptoxProyecto"
         managed= False
+
+
+
+class CobrosxFacturas(models.Model):
+    IDCobroxFactura = models.AutoField(primary_key=True)
+    FechaAlta = models.DateTimeField()
+    Total = models.DecimalField(default=0, max_digits=30, decimal_places=5)
+
+    class Meta:
+        db_table="CobrosxFacturas"
+
+
+
+class CobrosxCliente(models.Model):
+    IDCobro = models.AutoField(primary_key=True)
+    FechaAlta = models.DateTimeField()
+    Total = models.DecimalField(default=0, max_digits=30, decimal_places=5)
+    Folio = models.CharField(max_length=10, unique=True)
+    RutaPDF = models.CharField(max_length=300)
+    RutaXML = models.CharField(max_length=300)
+    FechaCobro = models.DateTimeField()
+
+    class Meta:
+        db_table="CobrosxCliente"
+
+
+
+class RelacionCobrosFacturasxCliente(models.Model):
+    IDRelacionCobroFacturasxCliente = models.AutoField(primary_key=True)
+    IDCobro = models.ForeignKey(CobrosxCliente, on_delete=models.CASCADE, db_column = 'IDCobro')
+    IDCobroxFactura = models.ForeignKey(CobrosxFacturas, on_delete=models.CASCADE, db_column = 'IDCobroxFactura')
+    IDFactura = models.ForeignKey(FacturasxCliente, on_delete=models.CASCADE, db_column = 'IDFactura')
+    IDUsuarioAlta = models.IntegerField(default=0)
+    IDCliente = models.IntegerField(default=0)
+
+    class Meta:
+        db_table="RelacionCobrosFacturasxCliente"
